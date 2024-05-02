9eeb98b915361c37cc7e9600ce83e69d61806846
Tomoyo Alvåg <tomoyo.tomoyo.m@gmail.com>
2024-05-02 14:12:08 +0200

    update

--- app/types.ts
+++
@@ -1,4 +0,0 @@
-import { ReactNode } from "react";
-
-export type PageProps = Readonly<{ params: { slug: string } }>;
-export type LayoutProps = Readonly<{ children: ReactNode }>;
--- app/utils/createRandomId.tsx
+++
@@ -1,3 +0,0 @@
-export default function createRandomId() {
-  return `id_${Date.now()}_${Math.random().toString(15).slice(2, 9)}`;
-}
